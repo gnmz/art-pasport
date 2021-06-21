6dad05a8588d931e6ebb3164e92649d33ea0ea74
gnmz <i.popov.developer@gmail.com>
2021-06-21 14:42:48 +0300

    add events location reducer to rootreducer

--- client/src/store/reducers/index.ts
+++ client/src/store/reducers/index.ts
@@ -1,7 +1,8 @@
 import { combineReducers } from "redux";
-
-
+import { eventsLocationReducer } from "./eventsLocation";
 
 export const rootReducer = combineReducers({
+  events: eventsLocationReducer,
+});
 
-})
+export type RootState = ReturnType<typeof rootReducer>;
